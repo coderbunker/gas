ce7929397ea19b9a3705c547a7f14a06e8f994b0
Ricky Ng-Adam <rngadam@gmail.com>
2018-04-25 16:30:05 +0800

    add change trigger

--- calendar/main.js
+++ calendar/main.js
@@ -1,3 +1,4 @@
+// registered in Edit > Current project trigger > From Calendar | Event Updated
 function transfer() {
   const calendars = CalendarApp.getCalendarsByName('Coderbunker Internal Projects Timesheet')
   const calendar = calendars[0];
@@ -46,3 +47,12 @@ function export(calendar) {
 
   return obj;
 }
+
+//https://developers.google.com/apps-script/guides/triggers/events
+//To synchronize with Calendar in Apps Script, perform the following steps:
+//Enable the Calendar advanced service for the script project. The built-in Calendar service isn't sufficent for this workflow.
+//Determine what calendars should be synchronized. For each such calendar, perform an intitial sync operation using the Calendar advanced service's Events.list() method.
+//The result of the initial sync returns a nextSyncToken for that calendar. Store this token for later use.
+//When the Apps Script EventUpdated trigger fires indicating a calendar event change, perform an incremental sync for the affected calendar using the stored nextSyncToken. This is essentially another Events.list() request, but providing the nextSyncToken limits the response to only events that have changed since the last sync.
+//Examine the response of the sync to learn what events were updated and have your code respond appropriately. For example, you can log the change, update a spreadsheet, send email notices, or take other actions.
+//Update the nextSyncToken you stored for that calendar with the one returned by the incremental sync request. This forces the next sync operation to only return the most current changes.
