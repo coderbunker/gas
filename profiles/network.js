e732c6beb3a37f31d0d7f50a8c64b81685293750
Ricky Ng-Adam <rngadam@gmail.com>
2018-05-04 18:05:21 +0800

    issue coderbunker/timesheet-backend#22: use apikey

--- profiles/network.js
+++ profiles/network.js
@@ -1,11 +1,14 @@
 function postData(id, doc, endpoint) {
+  const apikey = PropertiesService.getScriptProperties().getProperty('APIKEY');
   const options = {
     method : 'post',
     contentType: 'application/json',
     payload: JSON.stringify({
       id: id,
-      doc: doc
-    })
+      doc: doc,
+      apikey: apikey
+    }),
+    muteHttpExceptions: true
   };
   return UrlFetchApp.fetch(endpoint, options);
 }
