fbf2d61585c514e627c28ffeeeecab9a41bf84af
Anca <ancag.munteanu@gmail.com>
2018-10-29 10:59:01 +0800

    after rickys corrections

--- gas/sendEmail.js
+++ gas/sendEmail.js
@@ -7,33 +7,45 @@ function onOpen() {
 }
 
 function sendEmail() {
-  var file = DriveApp.getFileById('1WKqYD9GLqmLjDqJyeY4BPkvl31vgRAeM');
+  var file = DriveApp.getFileById('1KvZudA9OtE-PRPyY7kP567kjhWGJqUop');
+  var addDrive = DriveApp.getFileById('1Mh_uizD2jeJ72tIdQiD4Y8nzwi-CsozD');
   
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var sheet = spreadsheet.getSheetByName("Emails");
-  var startRow = 2; // First row of data to process
-  var numRows = sheet.getLastRow(); // Number of rows to process 
+  var firstRowToProcess = 2; // First row of data to process
+  var numRowsToProcess = sheet.getLastRow(); // Number of rows to process 
   // Fetch the range of cells (row, column, numRows, numColumns--this check column 3 is it is duplicate)
-  var dataRange = sheet.getRange(startRow, 1, numRows - 1, 3);
+  const SENT_COLUMN_INDEX = 3;
+  var dataRange = sheet.getRange(firstRowToProcess, 1, numRowsToProcess - 1, SENT_COLUMN_INDEX);
   // Fetch values for each row in the Range.
   var data = dataRange.getValues();
   for (var i = 0; i < data.length; ++i) {
     var row = data[i];
     var name = row[0]; // First column
     var email = row[1]; // Second column
-    var isBlank = sheet.getRange(startRow + i, 3, 1, 1).isBlank();
+    var isBlank = sheet.getRange(firstRowToProcess + i, SENT_COLUMN_INDEX, 1, 1).isBlank();
     if (isBlank) { // Prevents sending duplicates
-      var html = HtmlService.createHtmlOutputFromFile('emailTemplate').getContent();
+      var htmlTemplate = HtmlService.createHtmlOutputFromFile('emailTemplate');
+      var htmlContent = htmlTemplate.getContent();
+      
       var subject = name + "'s " + "Coderbunker Onboarding";
   
-      GmailApp.sendEmail(email, subject, "", 
-                     {
-                       "from":"services@coderbunker.com", "name": "Coderbunker Services", 
-                       "cc":"bizdev@coderbunker.com",
-                       htmlBody: html, 
-                       inlineImages: {imageKey: file.getAs(MimeType.PNG)},
-                      });
-      sheet.getRange(startRow + i, 3).setValue(new Date());
+      GmailApp.sendEmail(
+        email, 
+        subject, 
+        "", 
+        {
+          "from":"services@coderbunker.com", 
+          "name": "Coderbunker Services", 
+          "cc":"bizdev@coderbunker.com",
+          htmlBody: htmlContent, 
+          inlineImages: {
+            imageKey: file.getAs(MimeType.PNG), 
+            addDrive: addDrive.getAs(MimeType.PNG)
+          },
+        }
+      );
+      sheet.getRange(firstRowToProcess + i, 3).setValue(new Date());
       // Make sure the cell is updated right away in case the script is interrupted
       SpreadsheetApp.flush();
 
